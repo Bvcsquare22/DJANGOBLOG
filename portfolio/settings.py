4b5b04816625dd17e4c89b569e4404a0b0291e93
Denis Anatoly <denisnatoly83@gmail.com>
2025-10-16 14:09:37 +0100

    initial commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3 ==>postgresql',
+        'NAME': "railway",
+        'USER': "postgres",
+        'PASSWORD': "IzTCqzFkanScMfqUvAWIDTQvlHWHjWmO",
+        'HOST': "gondola.proxy.rlwy.net",
+        'PORT': "43679"
+
     }
 }
 
